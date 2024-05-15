01f17f55e24d0c46e33733eae069f3c3eda4a276
YukhnikOlga <olga.iukhnik@gmail.com>
2024-05-15 09:29:02 +0300

    parameterized test

--- src/test/java/org/example/cyclesqa/services/RestServiceTest.java
+++ src/test/java/org/example/cyclesqa/services/RestServiceTest.java
@@ -2,34 +2,42 @@ package org.example.cyclesqa.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestServiceTest {
 
-    @Test
-    public void testRestIncome10_000() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    public void testRestIncome10_000(int expected, int income, int expenses, int threshold) {
         RestService service = new RestService();
 
-        int income = 10_000;  // заработано за текущий месяц
-        int expenses = 3_000;  // ежемесячные расходы
-        int threshold = 20_000; // мин. сумма,чтобы след. месяц не работать.
-        int expected = 3; // кол-во месяцев отдыха
+//        int income = 10_000;  // заработано за текущий месяц
+//        int expenses = 3_000;  // ежемесячные расходы
+//        int threshold = 20_000; // мин. сумма,чтобы след. месяц не работать.
+//        int expected = 3; // кол-во месяцев отдыха
 
         int actual = service.calcRest(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
 
-    }
+ }
 
-    @Test
-    public void testRestIncome100_000() {
-        RestService service = new RestService();
-
-        int income = 100_000;  // заработано за текущий месяц
-        int expenses = 60_000;  // ежемесячные расходы
-        int threshold = 150_000; // мин. сумма,чтобы след. месяц не работать.
-        int expected = 2; // кол-во месяцев отдыха
-
-        int actual = service.calcRest(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-
-    }
+//    @Test
+//    public void testRestIncome100_000() {
+//        RestService service = new RestService();
+//
+//        int income = 100_000;  // заработано за текущий месяц
+//        int expenses = 60_000;  // ежемесячные расходы
+//        int threshold = 150_000; // мин. сумма,чтобы след. месяц не работать.
+//        int expected = 2; // кол-во месяцев отдыха
+//
+//        int actual = service.calcRest(income, expenses, threshold);
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
 }
